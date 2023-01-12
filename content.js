1929ccbe933eae5bc6be27aaf5c95cab09cbc34b
stphnl <stphn.l.nyc@gmail.com>
2023-01-12 13:15:07 -0500

    working on DOM manipulation

--- content.js
+++ content.js
@@ -5,4 +5,14 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
+// console.log(window.location.href);
+// alert('Hello world from ' + document.title); //Hello world from https://www.newyorker.com
 
+
+// console.log('Content script works!');
+// console.log('Must reload extension for modifications to take effect.');
+
+// const docTest = document.querySelector(".quick-link-container");
+// console.log(docTest);
+// console.log(window.location);
+// console.log(document.title)
--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 // Handles your frontend UI logic.
+document.addEventListener('DOMContentLoaded', () => {
+  const addButton = document.querySelector(".add-button");
+  const minusButton = document.querySelector(".minus-button");
+
+  addButton.addEventListener('click', (e) => {
+    console.log('addButton clicked');
+  });
+
+  minusButton.addEventListener('click', (e) => {
+    console.log('minusButton clicked');
+  });
+});
