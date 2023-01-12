73900833accb8474fc1cc760800897fcae9f9a45
stphnl <stphn.l.nyc@gmail.com>
2023-01-11 22:37:28 -0500

    needs more work lol

--- background.js
+++ background.js
@@ -5,3 +5,23 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the browser level activities (e.g. tab management, etc.)
 // License: MIT
+
+
+// Regex-pattern to check URLs against. 
+// It matches URLs like: http[s]://[...]stackoverflow.com[...]
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (true) {
+      console.log('browserAction');
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    }
+});
